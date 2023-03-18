2c04e367ba5a87dd808d876656c5e0689299df24
Dino Scheidt <github@dino-scheidt.de>
2023-03-18 02:29:41 +0100

    ✅ Changed case 4 to insight by @andrejilderda that globby, not micromatch, is used. See: https://github.com/storybookjs/storybook/issues/11181#issuecomment-1474347928
    Thanks!

--- tools/.storybook/main.ts
+++ tools/.storybook/main.ts
@@ -46,9 +46,7 @@ switch (issueCase) {
        * the libraries using their proper mono-repo identifiers. Renders
        * the stories but causes a recursion through the nested node_modules
        * (SampleView imports Component).
-       * -> TSDoc to addon-docs broken
-       * -> Recursion of nested symlink node_modules
-       * -> No live reload when adding new stories files (needs restart)
+       * -> Broken
        */
       "../node_modules/@repo/ui/**/*.stories.@(js|jsx|ts|tsx)",
       "../node_modules/@repo/views/**/*.stories.@(js|jsx|ts|tsx)",
@@ -57,16 +55,16 @@ switch (issueCase) {
   case "4_proper_workspace_stories_trying_to_avoid_recursion_with_glob":
     configStories = [
       /**
-       * Since storybook uses micromatch under the hood, trying to add a negative
-       * glob for nested node_modules via "!(/node_modules)".
-       * -> Not working. No other way to exclude nested node_modules except for
-       * manually passing files, but that would break hot-reloading when adding
-       * new stories files.
-       * See: https://github.com/storybookjs/storybook/issues/11181
-       * See: https://github.com/storybookjs/storybook/issues/15913
+       * Since storybook uses globby under the hood, trying to add a negative
+       * glob for nested node_modules via "**!(node_modules)/**!(node_modules)".
+       * -> Insight from https://github.com/storybookjs/storybook/issues/11181#issuecomment-1474347928
+       *    though sometimes just "**!(node_modules)" seems to work, when there
+       *    are stories directly on next level folder. The following wont work:
+       *    "../node_modules/@repo/views/**!(node_modules)/**!(node_modules)/*.stories.@(js|jsx|ts|tsx)"
+       *    This does:
+       *    "../node_modules/@repo/views/**!(node_modules)/*.stories.@(js|jsx|ts|tsx)"
        */
-      "../node_modules/@repo/ui/**!(/node_modules)/*.stories.@(js|jsx|ts|tsx)",
-      "../node_modules/@repo/views/**!(/node_modules)/*.stories.@(js|jsx|ts|tsx)",
+      "../node_modules/@repo/**!(node_modules)/**!(node_modules)/*.stories.@(js|jsx|ts|tsx)",
     ];
     break;
 }
